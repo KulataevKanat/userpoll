0e7978bd0672dab4aea48795f8abaf45a2e25e5c
KulataevKanat <RaketaEnot4466@gmail.com>
2021-09-22 04:16:40 +0600

    project exceptions

---
+++ userpoll/advice.py
@@ -0,0 +1,29 @@
+# exceptions
+USER_DETAIL = "user"
+ADMIN_DETAIL = "admin"
+DATA_DETAIL = "detail"
+ACCESS_TOKEN_DETAIL = "access_token"
+USER_NOT_FOUND = "Пользователь не найден."
+PAGE_NOT_FOUND = "Страница не найдена."
+USER_BLOCKED = "Пользвователь заблокирован."
+USER_NOT_ACTIVE = "Пользвователь не активен."
+WRONG_PASSWORD = "Неверное имя пользователя или пароль."
+AUTH_CREDENTIALS = "Учетные данные не были предоставлены."
+REFRESH_TOKEN_EXPIRED = "Срок действия токена-обновления истек, пожалуйста, войдите снова."
+ACCESS_TOKEN_EXPIRED = "Срок действия токена-доступа истек."
+TOKEN_PREFIX = "Префикс токена отсутствует."
+DATA_NOT_FOUND = "Данные не найдены."
+CREATE_POLL_DATE_ACTIVE = 'Невозможно добавить вопрос к опросу пока стоит "дата старта".'
+DELETE_POLL_DATE_ACTIVE = 'Невозможно удалить вопрос к опросу пока стоит "дата старта".'
+UPDATE_POLL_DATE_ACTIVE = 'Невозможно изменить вопрос к опросу пока стоит "дата старта".'
+
+# types
+TYPE_NONE = 'None'
+TEXT = 'Ответ текстом'
+ONE_OPTION = 'Ответ с выбором одного варианта'
+MANY_OPTIONS = 'Ответ с выбором нескольких вариантов'
+
+# answers
+ANSWER_NONE = 'None'
+YES = 'да'
+NO = 'нет'
